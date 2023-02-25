2a838245ce5028b9b52677ca937d9bd69cdb11f7
Jorge Alexandre <jorgeaamsantos@gmail.com>
2023-02-25 14:16:58 -0300

    add banner

--- src/pages/Paginainicial/index.jsx
+++ src/pages/Paginainicial/index.jsx
@@ -1,11 +1,21 @@
 import Cabecalho from "../../components/Cabecalho";
 import Menu from "../../components/Menu";
+import banner from './banner.png';
+import styles from './PaginaInicial.module.scss';
 
 export default function App() {
   return (
     <>
       <Cabecalho />
-      <Menu />
+      <main>
+        <section className={styles.principal}>
+          <Menu />
+          <div className={styles.principal__imagem}>
+            <h1>A galeria mais completa do espaço</h1>
+            <img src={banner} alt="Banner da terra vista do espaco" />
+          </div>
+        </section>
+      </main>
     </>
   );
 }
